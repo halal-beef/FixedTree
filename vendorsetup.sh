d1d194d2959607b9d0bdbdf731e5ce1776f9204c
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2021-11-13 15:35:33 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -39,7 +39,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    	export TW_DEFAULT_LANGUAGE="en"
 	export OF_KEEP_FORCED_ENCRYPTION=1
 	export OF_PATCH_AVB20=1
-        #export OF_SCREEN_H=2340
+        export OF_SCREEN_H=1520
         #export OF_STATUS_H=80
         #export OF_STATUS_INDENT_LEFT=48
         #export OF_STATUS_INDENT_RIGHT=48
